c2ad36b02f331f0d36177c99531b29dcc5d2575e
Magnus Løvhøiden <mlovhoiden@gmail.com>
2022-03-07 12:39:44 +0100

    Create socket

--- webserver.c
+++ webserver.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include "sem.h"
 #include "bbuffer.h"
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+   
 
 int main() {
-    printf("Hello\n");
+    int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
+   
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else {
+        printf("Socket successfully created..\n");
+
+        bzero(&servaddr, sizeof(servaddr));
+
+        // assign IP, PORT
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servaddr.sin_port = htons(PORT);
+    }
     return 0;
 }
