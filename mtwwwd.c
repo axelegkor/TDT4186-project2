61ab81a52c3385796d09abfe3d1bd9cf0c28e892
Axel Legallais-Korsbakken <axelegkor@gmail.com>
2022-03-15 16:16:20 +0100

    socket, bind, listener, accept

---
+++ mtwwwd.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "sem.h"
+#include "bbuffer.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/un.h>
+
+#define PORT 8000;
+
+int main() {
+    
+    int mainSocket;
+
+    mainSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (mainSocket == -1) {
+        printf("Socket failed to be created\n");
+        exit(0);
+    }
+    printf("Socket was successfully created!\n");
+
+    struct sockaddr_un my_addr, peer_addr;
+    socklen_t peer_addr_size;
+
+    memset(&my_addr, 0, sizeof(struct sockaddr_un));
+    
+    //my_addr.sun_family = AF_UNIX;
+    //strncpy(my_addr.sun_path, MY_SOCK_PATH, sizeof(my_addr.sun_path) - 1);
+
+
+    int binder;
+    binder = bind(mainSocket, (struct sockaddr *) &my_addr, sizeof(struct sockaddr_un));
+    if (binder == -1){
+        printf("Socket failed to be bound\n");
+        exit(0);
+    }
+    printf("Socket was successfully bound!\n");
+
+    int listener;
+    listener = listen(mainSocket, 128);
+    if (listener == -1){
+        printf("Socket failed to listen\n");
+        exit(0);
+    }
+    printf("Socket was successfully set as passive (listening)!\n");
+
+    peer_addr_size = sizeof(struct sockaddr_un);
+
+    int descriptor_accepted;
+    descriptor_accepted = accept(mainSocket, (struct sockaddr *) &peer_addr, &peer_addr_size);
+    if (descriptor_accepted == -1){
+        printf("Failed to accept incoming connection\n");
+        exit(0);
+    }
+    printf("Connection %d was accepted!", descriptor_accepted);
+
+    exit(0);
+
+} 
--- webserver.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "sem.h"
-#include "bbuffer.h"
-
-int main() {
-    printf("Hello\n");
-    return 0;
-}
