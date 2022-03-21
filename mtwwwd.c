6aa4db73f9108ee78faab5c15d96a40cc89c0616
mario <mariohar@stud.ntnu.no>
2022-03-21 09:46:40 +0100

    gjort ferdig noe kode

--- mtwwwd.c
+++ mtwwwd.c
@@ -9,29 +9,52 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define PORT 8000
 #define MAXREQ (4096*1024)
 
 char buffer[MAXREQ], body[MAXREQ], msg[MAXREQ];
 
-int main() {
+int main(int argc, char *argv[]) {
     
     int mainSocket;
+    char www_path[MAXREQ], path[1024];
+    FILE *fileOpner;
+    int threads, bufferSlots, portnumber;
+    struct sockaddr_in serverAddress;
+    
+    if (argv[1]) {
+        strcpy(www_path, argv[1]);
+        printf("Serving the path: %s\n", www_path);
+    }
+    else {
+        //printf("Did not find the www_path.\n");
+        exit(0);
+    } 
+    if (argv[2]) {
+        portnumber = atoi(argv[2]);
+        printf("Portnumber: %d\n", portnumber);
+
+    }
+    if (argv[3]) {
+        threads = atoi(argv[3]);
+        printf("Threds: %d\n", threads);
+
+    }
+    bufferSlots = atoi(argv[4]);
+    printf("Bufferslots: %d\n", bufferSlots);
+
 
     mainSocket = socket(PF_INET, SOCK_STREAM, 0);
     if (mainSocket == -1) {
         printf("Socket failed to be created\n");
         exit(0);
     }
-    printf("Socket was successfully created!\n");
-
-    struct sockaddr_in serverAddress;
+    printf("The socket was successfully created!\n");
 
     bzero(&serverAddress, sizeof(serverAddress));
 
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddress.sin_port = htons(PORT);
+    serverAddress.sin_port = htons(portnumber);
 
     if (bind(mainSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1){
         printf("Socket failed to be bound\n");
@@ -39,7 +62,7 @@ int main() {
     }
     printf("Socket was successfully bound!\n");
 
-    bzero(&serverAddress, sizeof(serverAddress));
+    // bzero(&serverAddress, sizeof(serverAddress));
 
     if (listen(mainSocket, 128) == -1){
         printf("Socket failed to listen\n");
@@ -49,8 +72,7 @@ int main() {
 
     struct sockaddr_in clientAddress;
     socklen_t clientLenght;
-    int newSocket;
-    int saved;
+    int newSocket, saved;
 
     while(1) {
         clientLenght = sizeof(clientAddress);
@@ -59,13 +81,40 @@ int main() {
             printf("Failed to accept incoming connection\n");
             exit(0);
         }
+        printf("Connection was accepted.\n");
         bzero(buffer, sizeof(buffer));
+
         saved = read(newSocket, buffer, sizeof(buffer) - 1);
         if (saved == -1) {
             printf("Failed to read from socket");
         }
-        snprintf (body, sizeof (body),
-        "<html>\n<body>\n <h1>Hello web browser</h1>\nYour request was\n <pre>%s</pre>\n </body>\n</html>\n", buffer);
+
+        // Finding the right path
+        strcpy(path, www_path);
+        strtok(buffer, " ");
+        strcat(path, strtok(NULL, " "));
+
+        fileOpner = fopen(path, "r");
+        printf("The finale path: %s\n", path);
+
+        if (fileOpner == NULL) 
+             fileOpner = fopen("404page.html", "r");
+
+        if (fileOpner != NULL) {
+        size_t newLen = fread(body, sizeof(char), MAXREQ, fileOpner);
+        if ( ferror( fileOpner ) != 0 ) {
+            fputs("Error reading file", stderr);
+        } else {
+            body[newLen++] = '\0'; /* Just to be safe. */
+        }
+        fclose(fileOpner);
+        }
+        // if (s != NULL) {
+        //     path = malloc(strlen(www_path) + strlen(s) + 1);
+        //     strcpy(path, www_path);
+        //     strcpy(path, s);
+        // }
+        // read_file(path, body);
 
         snprintf (msg, sizeof (msg),
         "HTTP/1.0 200 OK\n"
