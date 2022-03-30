e4c07ba02a12066134bdb9e7208c9a4a58c08a6e
Axel Legallais-Korsbakken <axelegkor@gmail.com>
2022-03-30 13:05:19 +0200

    remove unused import

--- PE3/flush.c
+++ PE3/flush.c
@@ -2,56 +2,63 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-#include<sys/wait.h>
-#include <readline/readline.h>
+#include <sys/wait.h>
 
-
-void print_shell() 
+void print_shell()
 {
     printf("\n\n\n******************"
-        "************************");
+           "************************");
     printf("\n\n\n****The famous little unix shell****");
     printf("\n\n\t-USE AT YOUR OWN RISK-");
     printf("\n\n\n*******************"
-        "***********************\n\n");
+           "***********************\n\n");
 }
 
-void handle_input(char input[256]) 
-{  // char buff[] = "kim-tae-yeon";
+void handle_input(char input[256])
+{ // char buff[] = "kim-tae-yeon";
 
     int i = 0;
     char *usr_input = strtok(input, " ");
     char *data[2];
 
-    while (usr_input) {
+    while (usr_input)
+    {
         data[i++] = usr_input;
         usr_input = strtok(NULL, " ");
     }
 
-    for (i = 0; i < 2; i++) {
+    for (i = 0; i < 2; i++)
+    {
         printf("%s\n", data[i]);
     }
 }
 
-void syscmd_exec(char** command) 
+void syscmd_exec(char **command)
 {
     pid_t pid = fork();
 
-    if (pid < 0) {
+    if (pid < 0)
+    {
         printf("Could not fork a child.\n");
         return;
-    } else if (pid == 0) {
-        if (execvp(command[0], command) < 0) {
+    }
+    else if (pid == 0)
+    {
+        if (execvp(command[0], command) < 0)
+        {
             printf("The command coudl not be executed.\n");
-        } exit(0);
-    } else {
+        }
+        exit(0);
+    }
+    else
+    {
         wait(NULL);
         return;
     }
 }
 
 int main()
-{    
+{
     // Finds the current current working directory
     char path[200];
     getcwd(path, 200);
@@ -59,7 +66,7 @@ int main()
     char input_str[256];
 
     // print_shell();
-    
+
     printf("%s: ", path);
     fgets(input_str, 256, stdin);
 
