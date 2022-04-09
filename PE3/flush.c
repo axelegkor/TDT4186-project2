b02bf61341e3812dfe444651f37e7114d2a3b012
mario <mariohar@stud.ntnu.no>
2022-04-09 16:37:06 +0200

    can add multiple args

--- PE3/flush.c
+++ PE3/flush.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
+#define BUFFER_SIZE 256
+
+char *handeled_input[BUFFER_SIZE];
+
 void print_shell()
 {
     printf("\n\n\n******************"
@@ -17,17 +21,31 @@ void print_shell()
 void print_dir() 
 {
     // Finds the current current working directory
-    char cwd[256];
+    char cwd[BUFFER_SIZE];
     getcwd(cwd, sizeof(cwd));
     printf("%s", cwd);
 }
 
+void handle_cd(char *handled_input[])
+{
+    if (strcmp(handled_input[0], "cd") == 0)
+    {
+        if (strlen(handled_input[1]) == 0)
+        {
+            printf("The path is empty.\n");
+            return;
+        }
+        printf("Dir: %s\n", handled_input[1]);
+        printf("Correct\n");
+        chdir(handled_input[1]); // Changes the dir to the one stored in data[1]
+    }
+}
 
-void handle_input(char input[256], char* strings[], size_t size)
+void handle_input(char input[BUFFER_SIZE])
 { 
     int i = 0;
     char *usr_input = strtok(input, " ");
-    char *data[2]; // Contains the input from the user, data[0]: commands and data[1]: arguments
+    char *data[BUFFER_SIZE]; // Contains the input from the user, data[0]: commands and data[1]: arguments
 
     while (usr_input)
     {
@@ -36,26 +54,26 @@ void handle_input(char input[256], char* strings[], size_t size)
     }
 
     // Handles if the argument contains "\n"
-    strtok(data[1], "\n");   // Cleaner way to handle this: https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+    strtok(data[1], "\n\r");   // Cleaner way to handle this: https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
 
-    if (strcmp(data[0], "cd") == 0)
+    int empty_index = 0;    
+    for (int x = 0; x < 255; x++)
     {
-        if (strlen(data[1]) == 0)
+        if (data[x] == NULL) 
         {
-            printf("The path is empty.\n");
-            return;
+            empty_index = x;
+            break;
         }
-
-        printf("Dir: %s\n", data[1]);
-        printf("Correct\n");
-        chdir(data[1]); // Changes the dir to the one stored in data[1]
     }
-
-    if (size >= 3)
+    
+    for (int q = 0; q < empty_index - 1; q++)
     {
-        strings[0] = data[0];
-        strings[1] = data[1];
-        strings[3] = NULL;
+        handeled_input[q] = data[q];
+    }
+    handeled_input[empty_index] = NULL;
+
+    for (int p = 0; p < 3; p++){
+        printf("%d: %s\n", p, handeled_input[p]);
     }
 }
 
@@ -98,18 +116,19 @@ void syscmd_exec(char **command)
 
 int main()
 {
-    char input_str[256];
-    char *handeled_input[3];
+    char input_str[BUFFER_SIZE];
 
     while (1) 
     {
         print_dir();
         printf(": ");
-        fgets(input_str, 256, stdin);
+        fgets(input_str, BUFFER_SIZE, stdin);
 
-        handle_input(input_str, handeled_input, sizeof(handeled_input));
+        handle_input(input_str);
 
-        if (strcmp(handeled_input[0], "cd") != 0)
+        if (strcmp(handeled_input[0], "cd") == 0)
+            handle_cd(handeled_input);
+        else
             syscmd_exec(handeled_input);
     }
 
